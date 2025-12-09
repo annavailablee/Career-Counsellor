495316b7816c8ce3bf03a049773778251cc4f213
Anayya <anayya685@gmail.com>
2025-12-09 13:31:15 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
-import './index.css'; // This imports your styles
+import './index.css'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App /> {/* App is now rendered directly */}
+      <App /> 
     </BrowserRouter>
   </React.StrictMode>,
 );
